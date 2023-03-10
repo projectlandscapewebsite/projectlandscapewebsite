d3037640e792f9fc8e1a4ae4c59947b50d690096
aaronjtonner <tonneraaron@gmail.com>
2023-03-09 18:38:08 -0700

    site

--- src/components/info.js
+++ src/components/info.js
@@ -1,6 +1,6 @@
 export const Info = {
   name: "Project Landscape Ltd.",
-  hours: "9:00 AM - 5:00 PM Monday to Friday",
+  hours: "9:00 AM - 5:00 PM Monday - Friday",
 
   tel1: "403-257-4059",
   mail1: "info@projectlandscape.ca",
